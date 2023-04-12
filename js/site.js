3f8e77e238a9b1408e35ebf3d109591402052225
Khala Wright <khalawright5@gmail.com>
2023-04-12 11:47:34 -0400

    update to even numbers

--- js/site.js
+++ js/site.js
@@ -1,44 +1,45 @@
-
 //get the start and end values from the inputs
 //Entry point AKA controller function
 function getValues() {
-    let startValue = document.getElementById('startValue').value;
-    let endValue = document.getElementById('endValue').value;
+  let startValue = document.getElementById("startValue").value;
+  let endValue = document.getElementById("endValue").value;
 
-    let startNumber = parseInt(startValue); //startNumber = 0
-    //parseint forces the value to be an integer; this is because any info from html is text by default
-    let endNumber = parseInt(endValue); //startNumber = 100
+  let startNumber = parseInt(startValue); //startNumber = 0
+  //parseint forces the value to be an integer; this is because any info from html is text by default
+  let endNumber = parseInt(endValue); //startNumber = 100
 
-   let numberArray = generateNumbers(startNumber, endNumber);
-   displayNumbers(numberArray);
-
+  let numberArray = generateNumbers(startNumber, endNumber);
+  displayNumbers(numberArray);
 }
 
 // generate the range of numbers to display
 //Business/logic function
 function generateNumbers(start, end) {
-    
-    let basketOfNumbers = [];
+  let basketOfNumbers = [];
 
-    //loop!
-    for (let number = start; number <= end; number = number + 1) {
-        basketOfNumbers.push(number);
-    }
+  //loop!
+  for (let number = start; number <= end; number++) {
+    basketOfNumbers.push(number);
+  }
 
-    return basketOfNumbers; // [0, 1, 2, 3, 4, 5...., 100]
+  return basketOfNumbers; // [0, 1, 2, 3, 4, 5...., 100]
 }
 
 //placing the generated numbers on the page AND BOLD even numbers
 //View function
-function displayNumbers(numbers) { 
-    let results = '';
+function displayNumbers(numbers) {
+  let results = "";
 
-    for (let index = 0; index < numbers.length; index = index + 1) {
-        let currentNumber = numbers[index];
+  for (let index = 0; index < numbers.length; index = index + 1) {
+    let currentNumber = numbers[index];
 
-        results = results + `<tr><td>${currentNumber}</td></tr>`;
+    if (currentNumber % 2 == 0) {
+      results += `<tr><td class="evenNumber">${currentNumber}</td></tr>`;
+    } else {
+      results += `<tr><td>${currentNumber}</td></tr>`;
     }
+  }
 
-    let tableBody = document.getElementById('results');
-    tableBody.innerHTML = results;
+  let tableBody = document.getElementById("results");
+  tableBody.innerHTML = results;
 }
